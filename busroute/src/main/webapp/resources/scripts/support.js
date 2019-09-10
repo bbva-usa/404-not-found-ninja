97d451f0843d57212949167467acdbf769712d13
Narayana Chiluka <narayanareddy.chiluka@bbva.com>
2019-09-10 13:56:08 -0500

    initial push

--- busroute/src/main/webapp/resources/scripts/support.js
+++ busroute/src/main/webapp/resources/scripts/support.js
@@ -12,6 +12,9 @@ app.config(function($routeProvider) {
 	$routeProvider.when("/login", {
 		templateUrl : "login.html"
 			});
+	$routeProvider.when("/adminpage", {
+		templateUrl : "adminpage.html"
+			});
 
 });
 
@@ -25,5 +28,19 @@ app.controller('dashboardController', function($scope, $http, $location){
 app.controller('mapViewController' , function($scope, $location){
 	$busStopsFHPS = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
 	$busStopsGOES = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
+
+});
+
+
+
+app.controller('loginController', function($scope, $http, $location){
+	$scope.adminuser="admin";
+	$scope.password="password";
+	$scope.adminLogged=false;
+	$scope.login = function(username,password)
+	{
+		if(username==$scope.adminuser && password==$scope.password)
+			$scope.adminLogged=true;
+	}
 
 });
