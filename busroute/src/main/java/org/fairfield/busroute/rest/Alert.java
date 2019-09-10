622e4bfc44219f5529381d195a9ec9ad4b807c4d
Narayana Chiluka <narayanareddy.chiluka@bbva.com>
2019-09-10 16:21:52 -0500

    initial push for rest api

---
+++ busroute/src/main/java/org/fairfield/busroute/rest/Alert.java
@@ -0,0 +1,25 @@
+package org.fairfield.busroute.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Alert {
+	List <String> a = new ArrayList<String>();
+
+	@RequestMapping(value = "/alerts", method = RequestMethod.GET )
+		public List<String> getAlertsList() {
+		return a;
+	}
+	
+	@RequestMapping(value = "/alerts/{alertMessage}", method = RequestMethod.POST)
+	public void putAlertMessage (@PathVariable String alertMessage) {
+		a.add(alertMessage);
+	}
+}
