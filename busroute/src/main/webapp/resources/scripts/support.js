42e7d0368b375d37b37343773aab8d93b9d58515
Meneses, Sebastian <sebastian.menesespilco@bbva.com>
2019-09-10 12:21:30 -0500

    dash and base controller

--- busroute/src/main/webapp/resources/scripts/support.js
+++ busroute/src/main/webapp/resources/scripts/support.js
@@ -25,3 +25,13 @@ app.config(function($routeProvider) {
 		});
 
 });
+
+app.controller('dashboardController', function($scope, $http, $location){
+
+});
+
+app.controller('mapViewController' , function($scope, $location){
+	$busStopsFHPS = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
+	$busStopsGOES = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
+
+});
