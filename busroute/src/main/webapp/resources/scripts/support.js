ae3eca6b04e10d831ca9cdd2d60ef5826ac0c405
Narayana Chiluka <narayanareddy.chiluka@bbva.com>
2019-09-10 12:46:32 -0500

    initial push

--- busroute/src/main/webapp/resources/scripts/support.js
+++ busroute/src/main/webapp/resources/scripts/support.js
@@ -1,31 +1,23 @@
-/**
- * 
- */
+
 
 var app = angular.module('busRouteApp', [ "ngRoute" ]);
 
-app.controller('indexcontroller', function($scope, $http, $location) {
 
-});
 
 app.config(function($routeProvider) {
 	$routeProvider.when("/", {
 	templateUrl : "dashboard.html",
 
 		});
-	$routeProvider.when("/packageView", {
-	templateUrl : "viewPackageContent.html"
-		});
-
-	$routeProvider.when("/fileEdit", {
-	templateUrl : "editFile.html"
-		});
-	$routeProvider.when("/fileView", {
-	templateUrl : "viewFileContent.html"
-		});
+	$routeProvider.when("/login", {
+		templateUrl : "login.html"
+			});
 
 });
 
+app.controller('indexcontroller', function($scope, $http, $location) {
+
+});
 app.controller('dashboardController', function($scope, $http, $location){
 
 });
