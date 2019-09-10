63f38d7abb31a626d1b484f3e1e815fd577f9299
Meneses, Sebastian <sebastian.menesespilco@bbva.com>
2019-09-10 12:50:22 -0500

    update

--- busroute/src/main/webapp/resources/scripts/support.js
+++ busroute/src/main/webapp/resources/scripts/support.js
@@ -23,7 +23,82 @@ app.controller('dashboardController', function($scope, $http, $location){
 });
 
 app.controller('mapViewController' , function($scope, $location){
-	$busStopsFHPS = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
-	$busStopsGOES = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
+	var busStopsFHPS = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
+	var busStopsGOES = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
+
+	var centerMap = {lat:33.4859402, lng:-86.9119366}
+	var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+	function addMarker(position, map){
+		let marker = new google.maps.Marker({
+  		position:position,
+  		icon:image,
+  		map: map
+  	});
+	}
+
+	function initMap(center, busStops) {
+		var directionsService = new google.maps.DirectionsService();
+		var directionsRenderer = new google.maps.DirectionsRenderer();
+
+  	var myCenter =  center;
+  	var map = new google.maps.Map(
+  	document.getElementById('map'), {zoom: 12, center: myCenter});
+
+  	for (i=0; i<size.busStops;i++){
+  		let position = {lat:busStops[i].Lat, lng:busStops[i].Long};
+  		addMarker(position,map);
+  	}
+
+
+
+  	var marker = new google.maps.Marker({
+  		position:myCenter,
+  		icon:image,
+  		map: map
+  	});
+
+ 		 directionsRenderer.setMap(map);
+
+
+ 		 map.addListener('center_changed', function() {
+          // 3 seconds after the center of the map has changed, pan back to the
+          //calculateAndDisplayRoute(directionsService, directionsRenderer);
+          // marker.
+          window.setTimeout(function() {
+          	map.panTo(marker.getPosition());
+          }, 3000);
+        });
+
+  	marker.addListener('click', function() {
+  		map.setZoom(15);
+  		map.setCenter(marker.getPosition());
+  	});
+
+
+  	var onChangeHandler = function() {
+  		calculateAndDisplayRoute(directionsService, directionsRenderer);
+  	};
+        //document.getElementById('start').addEventListener('change', onChangeHandler);
+        //document.getElementById('end').addEventListener('change', onChangeHandler);
+  }
+
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  	directionsService.route(
+  	{
+  		origin: {query: 'chicago, il'},
+  		destination: {query: 'st louis, mo'},
+  		travelMode: 'DRIVING'
+  	},
+  	function(response, status) {
+  		if (status === 'OK') {
+  			directionsRenderer.setDirections(response);
+  		} else {
+  			window.alert('Directions request failed due to ' + status);
+  		}
+  	});
+	}
+
+
 
 });
