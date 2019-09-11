7bafa85fb0d5e43c3d246a99b123fdec2e04cfbf
Mukherjee, Rohit <Rohit.mukherjee@bbva.com>
2019-09-10 20:53:37 -0500

    maps routes

--- busroute/src/main/webapp/resources/scripts/support.js
+++ busroute/src/main/webapp/resources/scripts/support.js
@@ -1,62 +1,51 @@
 function initMap() {
-  var directionsService = new google.maps.DirectionsService();
-  var directionsRenderer = new google.maps.DirectionsRenderer();
-  var myCenter =  {lat: 33.474912, lng: -86.910001};
-  var routes = [{"Time":"7:30 A.M.","Address":"Ave. C and 61st Street Back side of","Lat":"33.4984164","Long":"-86.8967907"},
-  {"Time":"7:33 A.M.","Address":"Avenue C and Jerry D. Coleman","Lat":"33.4758812","Long":"-86.9173647"},
-  {"Time":"7:35 A.M.","Address":"61st Street and Myron Massey Blvd.","Lat":"33.4769925","Long":"-86.9094935"},
-  {"Time":"7:32 A.M.","Address":"60th Street and Myron Massey Blvd.","Lat":"33.4771589","Long":"-86.9093703"}];
-  // The map, centered at Uluru
-  var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: myCenter});
+      var directionsService = new google.maps.DirectionsService;
+      var directionsRenderer = new google.maps.DirectionsRenderer;
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: {lat: 32.318230, lng:  -86.902298}
+      });
+      directionsRenderer.setMap(map);
 
-let marker = new google.maps.Marker({
-      position:myCenter,
-      icon:image,
-      map: map
-    });
-
-  var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-
-  function addMarker(position, map){
-   let marker = new google.maps.Marker({
-      position:position,
-      icon:image,
-      map: map
-    });
-  };
-   directionsRenderer.setMap(map);
-
-  let route = [];
-  for (key in routes) {
-   route.push(routes[key].Lat+ ','+ routes[key].Long);
-   console.log('routes: ', route);
- };
-
- for(var i =0; i<route.length;i++){
-   console.log('latlongs: ', route, i);
-   addMarker(route[i],map);
-   calculateAndDisplayRoute(directionsService, directionsRenderer, route[i],route[i+1]);
- };
-
- map.setZoom(15);
- map.setCenter(marker.getPosition());
-
+     
+   window.onload = calculateAndDisplayRoute(directionsService, directionsRenderer);
 
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer, source, destination) {
-  directionsService.route(
-  {
-    origin: {query: source},
-    destination: {query: destination},
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+
+   var routes = [{"Time":"7:30 A.M.","Address":"Ave. C and 61st Street Back side of","Lat":"33.4984164","Long":"-86.8967907"},
+      {"Time":"7:33 A.M.","Address":"Avenue C and Jerry D. Coleman","Lat":"33.4758812","Long":"-86.9173647"},
+      {"Time":"7:35 A.M.","Address":"61st Street and Myron Massey Blvd.","Lat":"33.4769925","Long":"-86.9094935"},
+      {"Time":"7:32 A.M.","Address":"60th Street and Myron Massey Blvd.","Lat":"33.4771589","Long":"-86.9093703"}];
+   
+  let start = [];
+  let end = [];
+  let waypts = [];
+  start.push(parseFloat(routes[0].Lat)+ ','+ parseFloat(routes[0].Long));
+  end.push(parseFloat(routes[routes.length-1].Lat)+ ','+ parseFloat(routes[routes.length-1].Long));
+  
+  
+  for (let j =1; j<routes.length-1;j++) {
+    waypts.push({
+      location: parseFloat(routes[j].Lat)+ ','+ parseFloat(routes[j].Long),
+      stopover: true
+    });
+   
+  };
+  console.log('routes: ', start, end, waypts);
+  directionsService.route({
+    origin: start[0],
+    destination: end[0],
+    waypoints: waypts,
+    optimizeWaypoints: true,
     travelMode: 'DRIVING'
-  },
-  function(response, status) {
+  }, function(response, status) {
     if (status === 'OK') {
       directionsRenderer.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
   });
 };
 
@@ -91,18 +80,18 @@ app.service('busses', function(){
 
 app.config(function($routeProvider) {
 	$routeProvider.when("/", {
-	templateUrl : "dashboard.html",
+		templateUrl : "dashboard.html",
 
-		});
+	});
 	$routeProvider.when("/login", {
 		templateUrl : "login.html"
-			});
+	});
 	$routeProvider.when("/adminpage", {
 		templateUrl : "adminpage.html"
-			});
+	});
 	$routeProvider.when("/mapview", {
 		templateUrl : "mapView.html"
-			});
+	});
 
 
 
@@ -121,11 +110,11 @@ app.controller('dashboardController', function($scope, $http, $location, busses)
 	$scope.listAlerts = [];
 
 	$scope.listBusses = [
-    { id: 'BOE', name: 'Yellow'},
-    { id: 'DE9', name: 'Green'},
-    { id: 'GOES', name: 'Orange'},
-    { id: 'RES', name: 'Pink'},
-    { id: 'FHPS', name: 'Blue'}
+	{ id: 'BOE', name: 'Yellow'},
+	{ id: 'DE9', name: 'Green'},
+	{ id: 'GOES', name: 'Orange'},
+	{ id: 'RES', name: 'Pink'},
+	{ id: 'FHPS', name: 'Blue'}
 	];
 
 	function setBus(bus){
@@ -138,84 +127,7 @@ app.controller('dashboardController', function($scope, $http, $location, busses)
 });
 
 app.controller('mapViewController' , function($scope, $location){
-	var busStopsFHPS = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
-	var busStopsGOES = JSON.parse('{["Time" : "7:30 A.M.","Address" :	"Ave. C and 61st Street Back side of","Lat"	:"33.4984164","Long" : "-86.8967907"]}');
-
-	var centerMap = {lat:33.4859402, lng:-86.9119366}
-	var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-
-	function addMarker(position, map){
-		let marker = new google.maps.Marker({
-  		position:position,
-  		icon:image,
-  		map: map
-  	});
-	}
-
-	function initMap(center, busStops) {
-		var directionsService = new google.maps.DirectionsService();
-		var directionsRenderer = new google.maps.DirectionsRenderer();
-
-  	var myCenter =  center;
-  	var map = new google.maps.Map(
-  	document.getElementById('map'), {zoom: 12, center: myCenter});
-
-  	for (i=0; i<size.busStops;i++){
-  		let position = {lat:busStops[i].Lat, lng:busStops[i].Long};
-  		addMarker(position,map);
-  	}
-
-
-
-  	var marker = new google.maps.Marker({
-  		position:myCenter,
-  		icon:image,
-  		map: map
-  	});
-
- 		 directionsRenderer.setMap(map);
-
-
- 		 map.addListener('center_changed', function() {
-          // 3 seconds after the center of the map has changed, pan back to the
-          //calculateAndDisplayRoute(directionsService, directionsRenderer);
-          // marker.
-          window.setTimeout(function() {
-          	map.panTo(marker.getPosition());
-          }, 3000);
-        });
-
-  	marker.addListener('click', function() {
-  		map.setZoom(15);
-  		map.setCenter(marker.getPosition());
-  	});
-
-
-  	var onChangeHandler = function() {
-  		calculateAndDisplayRoute(directionsService, directionsRenderer);
-  	};
-        //document.getElementById('start').addEventListener('change', onChangeHandler);
-        //document.getElementById('end').addEventListener('change', onChangeHandler);
-  }
-
-  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  	directionsService.route(
-  	{
-  		origin: {query: 'chicago, il'},
-  		destination: {query: 'st louis, mo'},
-  		travelMode: 'DRIVING'
-  	},
-  	function(response, status) {
-  		if (status === 'OK') {
-  			directionsRenderer.setDirections(response);
-  		} else {
-  			window.alert('Directions request failed due to ' + status);
-  		}
-  	});
-	}
-
-
-
+	initMap();
 });
 
 
@@ -228,11 +140,11 @@ app.controller('loginController', function($scope, $http, $location,$window, log
 	$scope.login = function(username,password){
 		console.log("invoked");
 		if(username==$scope.adminuser && password==$scope.password)
-			{ 
+		{ 
 			$scope.adminLogged=true;
 			login.setLogin(true);
 			$window.location.href ="#!/adminpage";
-			}
+		}
 	};
 
 });  
