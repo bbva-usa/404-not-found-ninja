3804853b5a19d0efd05bfac8ec9890e051f93a04
Meneses, Sebastian <sebastian.menesespilco@bbva.com>
2019-09-11 12:32:24 -0500

    adding css

--- busroute/src/main/webapp/resources/scripts/support.js
+++ busroute/src/main/webapp/resources/scripts/support.js
@@ -227,6 +227,7 @@ app.controller('dashboardController', function($scope, $http, $location, busses,
 
 app.controller('mapViewController' , function($scope, $http, $location, busses, ampms, $timeout){
 	initMap();
+  $scope.selectedRoute = "Blue-(Morning)"
   var routeFHPS_AM = [{Time : "7:30 A.M.",Address :  "Ave. C and 61st Street Back side of",Lat  :"33.4984164",Long : "-86.8967907", Stop: "A" },{Time : "7:33 A.M.",Address :  "Avenue C and Jerry D. Coleman",Lat  :"33.4758812",Long : "-86.9173647", Stop: "B" },{Time : "7:35 A.M.",Address :  "61st Street and Myron Massey Blvd.",Lat  :"33.4769925",Long : "-86.9094935", Stop: "C" },{Time : "7:32 A.M.",Address :  "60th Street and Myron Massey Blvd.",Lat  :"33.4771589",Long : "-86.9093703", Stop: "D" },{Time : "7:33 A.M.",Address :  "Myron Massey Blvd. and 55th Place",Lat  :"33.4827151",Long : "-86.9092874", Stop: "E" },{Time : "7:35 A.M.",Address :  "Myron Massey Blvd. and 53rd Place",Lat  :"33.4823689",Long : "-86.9092869", Stop: "F" },{Time : "7:37 A.M.",Address :  "516 54th Street (Middle of the block)",Lat  :"33.4842922",Long : "-86.9128126", Stop: "G" },{Time : "7:41 A.M.",Address :  "54th and Avenue D",Lat  :"33.4825863",Long : "-86.9116957", Stop: "H" },{Time : "7:43 A.M.",Address :  "5174 Hillside Drive",Lat  :"33.4868579",Long : "-86.9139871", Stop: "I" },{Time : "7:44 A.M.",Address :  "721 Court G",Lat  :"33.5299801",Long : "-86.8849877", Stop: "J" },{Time : "7:50 A.M.",Address :  "Glen Oaks Elementary School",Lat  :"33.4912583",Long : "-86.9104421", Stop: "K"}];
   var routeFHPS_PM = [{Time : "3:45 P.M.",Address :  "Glen Oaks Elementary School",Lat  :"33.4912583",Long : "-86.9104421"},{Time : "3:46 P.M.",Address :  "5174 Hillside Drive",Lat  :"33.4868579",Long : "-86.9139871"},{Time : "3:49 P.M.",Address :  "721 Court G",Lat  :"33.5299801",Long : "-86.8849877"},{Time : "3:51 P.M.",Address :  "53rd Street and Court H",Lat  :"33.4804248",Long : "-86.9064393"},{Time : "3:52 P.M.",Address :  "Myron Massey Blvd. and Court H",Lat  :"33.482348",Long : "-86.9071609"},{Time : "3:53 P.M.",Address :  "Myron Massey Blvd. and 53rd Place",Lat  :"33.4823689",Long : "-86.9092869"},{Time : "3:54 P.M.",Address :  "Myron Massey and 55th Place",Lat  :"33.4827151",Long : "-86.9092874"},{Time : "3:57 P.M.",Address :  "516 54th Street (Middle of the block)",Lat  :"33.4842922",Long : "-86.9128126"},{Time : "3:58 P.M.",Address :  "52nd Street and Avenue D",Lat  :"33.485488",Long : "-86.91497"},{Time : "3:59 P.M.",Address :  "54th and Avenue D",Lat  :"33.4825863",Long : "-86.9116957"},{Time : "4:00 P.M.",Address :  "55th and Avenue D",Lat  :"33.482962",Long : "-86.912922"},{Time : "4:04 P.M.",Address :  "56TH Street and Terrace Avenue",Lat  :"33.4822222",Long : "-86.9186264"},{Time : "4:06 P.M.",Address :  "60th Street and Terrace Avenue",Lat  :"33.4773587",Long : "-86.9083505"},{Time : "4:07 P.M.",Address :  "Avenue C and Jerry D. Coleman",Lat  :"33.477358",Long : "-86.9061628"},{Time : "4:08 P.M.",Address :  "Avenue D and Jerry D. Coleman",Lat  :"33.4758712",Long : "-86.9159008"},{Time : "4:09 P.M.",Address :  "Court E and Jerry D. Coleman",Lat  :"33.4758549",Long : "-86.9110933"},{Time : "4:10 P.M.",Address :  "Court F and Jerry D. Coleman",Lat  :"33.4756233",Long : "-86.9099983"},{Time : "4:12 P.M.",Address :  "61st Street and Myron Massey Blvd.",Lat  :"33.476847",Long : "-86.9095797"},{Time : "4:16 P.M.",Address :  "60th Street and Myron Massey Blvd.",Lat  :"33.4771589",Long : "-86.9093703"}];
   var routeGOES_AM = [{Time: "7:21 A.M.", Address:  "Parkridge Ave and Westfield Drive", Lat:  "33.453686", Long: "-86.93382", Stop: "A"},{ Time: "7:22 A.M.", Address:  "929 Westfield Drive", Lat:  "33.4526138", Long: "-86.9353981", Stop: "B"},{ Time: "7:22 A.M.", Address:  "817 Westfield Drive", Lat:  "33.4551111", Long: "-86.9373155", Stop: "C"},{ Time: "7:24 A.M.", Address:  "619 Westfield Drive", Lat:  "33.4577531", Long: "-86.93802", Stop: "D"},{ Time: "7:26 A.M.", Address:  "1000 Hillandale Dr. (Oakland Dr. side)", Lat:  "33.4539601", Long: "-86.9403287", Stop: "E"},{ Time: "7:27 A.M.", Address:  "900 Glen Oaks Drive (Hillandale Dr. Side)", Lat:  "33.4563187", Long: "-86.9411618", Stop: "F"},{ Time: "7:27 A.M.", Address:  "1000 Glen Oaks Drive", Lat:  "33.4557422", Long: "-86.9398591", Stop: "G"},{ Time: "7:28 A.M.", Address:  "800 Glen Crest Dr. (Park Ave. side)", Lat:  "33.455494", Long: "-86.9389742", Stop: "H"},{ Time: "7:29 A.M.", Address:  "846 Park Ave.", Lat:  "33.4542869", Long: "-86.9353883", Stop: "I"},{ Time: "7:30 A.M.", Address:  "824 Fairfax Drive", Lat:  "33.4562005", Long: "-86.9335979", Stop: "J"},{ Time: "7:31 A.M.", Address:  "407 Glen Crest Drive", Lat:  "33.457477", Long: "-86.9330304", Stop: "K"},{ Time: "7:32 A.M.", Address:  "345 Glen Crest Drive", Lat:  "33.4575006", Long: "-86.931328", Stop: "L"},{ Time: "7:33 A.M.", Address:  "Glen Oaks Elementary School", Lat:  "33.4596563", Long: "-86.9301173", Stop: "M"}];
@@ -283,18 +284,20 @@ app.controller('mapViewController' , function($scope, $http, $location, busses, 
 
 $scope.update = function(item) {
    let refinedData = [];
-   console.log('refinedData: ', refinedData, item);
+
         for (let j =0; j<data.length-1;j++) {
-    console.log('data: ', data[j].AMPM);
+
           if(data[j].AMPM === item.name){
             refinedData.push(data[j]);
           }};
 
-        console.log('refinedData: ', refinedData);
+
   } 
 
  $scope.callinitMap = function(item){
    let selectedBus = item;
+
+   $scope.selectedRoute = selectedBus.name;
     if(selectedBus.id === "BOE"){
     if(selectedBus.AMPM === "AM")
        $scope.routes = routeBOE_AM;
@@ -358,7 +361,7 @@ $scope.listAlerts=data.data;
 $scope.alertExist=true;
 
 }
-console.log($scope.listAlerts);
+
   });
 
 $timeout(function(){
@@ -389,7 +392,7 @@ app.controller('loginController', function($scope, $http, $location,$window, $co
 		$window.location.href ="#!/adminpage";
 	}
 	$scope.login = function(username,password){
-		console.log("invoked");
+
 		if(username==$scope.adminuser && password==$scope.password)
 		{ 
 			$scope.adminLogged=true;
