57cf58aadf57b78054df291f887db508af1a2903
Meneses, Sebastian <sebastian.menesespilco@bbva.com>
2019-09-11 10:07:07 -0500

    added bus routes

--- busroute/src/main/webapp/resources/scripts/support.js
+++ busroute/src/main/webapp/resources/scripts/support.js
@@ -1,4 +1,8 @@
 function initMap() {
+  var routeA_AM = [{"Time" : "7:30 A.M.","Address" :  "Ave. C and 61st Street Back side of","Lat"  :"33.4984164","Long" : "-86.8967907"},{"Time" : "7:33 A.M.","Address" :  "Avenue C and Jerry D. Coleman","Lat"  :"33.4758812","Long" : "-86.9173647"},{"Time" : "7:35 A.M.","Address" :  "61st Street and Myron Massey Blvd.","Lat"  :"33.4769925","Long" : "-86.9094935"},{"Time" : "7:32 A.M.","Address" :  "60th Street and Myron Massey Blvd.","Lat"  :"33.4771589","Long" : "-86.9093703"},{"Time" : "7:33 A.M.","Address" :  "Myron Massey Blvd. and 55th Place","Lat"  :"33.4827151","Long" : "-86.9092874"},{"Time" : "7:35 A.M.","Address" :  "Myron Massey Blvd. and 53rd Place","Lat"  :"33.4823689","Long" : "-86.9092869"},{"Time" : "7:37 A.M.","Address" :  "516 54th Street (Middle of the block)","Lat"  :"33.4842922","Long" : "-86.9128126"},{"Time" : "7:41 A.M.","Address" :  "54th and Avenue D","Lat"  :"33.4825863","Long" : "-86.9116957"},{"Time" : "7:43 A.M.","Address" :  "5174 Hillside Drive","Lat"  :"33.4868579","Long" : "-86.9139871"},{"Time" : "7:44 A.M.","Address" :  "721 Court G","Lat"  :"33.5299801","Long" : "-86.8849877"},{"Time" : "7:50 A.M.","Address" :  "Glen Oaks Elementary School","Lat"  :"33.4912583","Long" : "-86.9104421"}];
+  var routeA_PM = [{"Time" : "3:45 P.M.","Address" :  "Glen Oaks Elementary School","Lat"  :"33.4912583","Long" : "-86.9104421"},{"Time" : "3:46 P.M.","Address" :  "5174 Hillside Drive","Lat"  :"33.4868579","Long" : "-86.9139871"},{"Time" : "3:49 P.M.","Address" :  "721 Court G","Lat"  :"33.5299801","Long" : "-86.8849877"},{"Time" : "3:51 P.M.","Address" :  "53rd Street and Court H","Lat"  :"33.4804248","Long" : "-86.9064393"},{"Time" : "3:52 P.M.","Address" :  "Myron Massey Blvd. and Court H","Lat"  :"33.482348","Long" : "-86.9071609"},{"Time" : "3:53 P.M.","Address" :  "Myron Massey Blvd. and 53rd Place","Lat"  :"33.4823689","Long" : "-86.9092869"},{"Time" : "3:54 P.M.","Address" :  "Myron Massey and 55th Place","Lat"  :"33.4827151","Long" : "-86.9092874"},{"Time" : "3:57 P.M.","Address" :  "516 54th Street (Middle of the block)","Lat"  :"33.4842922","Long" : "-86.9128126"},{"Time" : "3:58 P.M.","Address" :  "52nd Street and Avenue D","Lat"  :"33.485488","Long" : "-86.91497"},{"Time" : "3:59 P.M.","Address" :  "54th and Avenue D","Lat"  :"33.4825863","Long" : "-86.9116957"},{"Time" : "4:00 P.M.","Address" :  "55th and Avenue D","Lat"  :"33.482962","Long" : "-86.912922"},{"Time" : "4:04 P.M.","Address" :  "56TH Street and Terrace Avenue","Lat"  :"33.4822222","Long" : "-86.9186264"},{"Time" : "4:06 P.M.","Address" :  "60th Street and Terrace Avenue","Lat"  :"33.4773587","Long" : "-86.9083505"},{"Time" : "4:07 P.M.","Address" :  "Avenue C and Jerry D. Coleman","Lat"  :"33.477358","Long" : "-86.9061628"},{"Time" : "4:08 P.M.","Address" :  "Avenue D and Jerry D. Coleman","Lat"  :"33.4758712","Long" : "-86.9159008"},{"Time" : "4:09 P.M.","Address" :  "Court E and Jerry D. Coleman","Lat"  :"33.4758549","Long" : "-86.9110933"},{"Time" : "4:10 P.M.","Address" :  "Court F and Jerry D. Coleman","Lat"  :"33.4756233","Long" : "-86.9099983"},{"Time" : "4:12 P.M.","Address" :  "61st Street and Myron Massey Blvd.","Lat"  :"33.476847","Long" : "-86.9095797"},{"Time" : "4:16 P.M.","Address" :  "60th Street and Myron Massey Blvd.","Lat"  :"33.4771589","Long" : "-86.9093703"}];
+      
+
       var directionsService = new google.maps.DirectionsService;
       var directionsRenderer = new google.maps.DirectionsRenderer;
       var map = new google.maps.Map(document.getElementById('map'), {
@@ -7,7 +11,7 @@ function initMap() {
       });
       directionsRenderer.setMap(map);
 
-   window.onload = calculateAndDisplayRoute(directionsService, directionsRenderer);
+   window.onload = calculateAndDisplayRoute(directionsService, directionsRenderer, routeA_AM);
    
 }
 
@@ -26,12 +30,8 @@ function addSchool(position, map){
   map: map 
   });
 }
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-
-   var routes = [{"Time":"7:30 A.M.","Address":"Ave. C and 61st Street Back side of","Lat":"33.4984164","Long":"-86.8967907"},
-      {"Time":"7:33 A.M.","Address":"Avenue C and Jerry D. Coleman","Lat":"33.4758812","Long":"-86.9173647"},
-      {"Time":"7:35 A.M.","Address":"61st Street and Myron Massey Blvd.","Lat":"33.4769925","Long":"-86.9094935"},
-      {"Time":"7:32 A.M.","Address":"60th Street and Myron Massey Blvd.","Lat":"33.4771589","Long":"-86.9093703"}];
+function calculateAndDisplayRoute(directionsService, directionsRenderer, routes) {
+    
    
   let start = [];
   let end = [];
@@ -183,11 +183,10 @@ app.controller('dashboardController', function($scope, $http, $location, busses)
 
 app.controller('mapViewController' , function($scope, $location){
 	initMap();
-   $scope.routes = [
-{Time:"7:30 A.M.",Address:"Ave. C and 61st Street Back side of",Lat:"33.4984164",Long:"-86.8967907", Stop: "A"},
-{Time:"7:33 A.M.",Address:"Avenue C and Jerry D. Coleman",Lat:"33.4758812",Long:"-86.9173647", Stop: "B"},
-{Time:"7:35 A.M.",Address:"61st Street and Myron Massey Blvd.",Lat:"33.4769925",Long:"-86.9094935", Stop: "C"},
-{Time:"7:32 A.M.",Address:"60th Street and Myron Massey Blvd.",Lat:"33.4771589",Long:"-86.9093703", Stop: "D"}];
+  var routeA_AM = [{Time : "7:30 A.M.",Address :  "Ave. C and 61st Street Back side of",Lat  :"33.4984164",Long : "-86.8967907"},{Time : "7:33 A.M.",Address :  "Avenue C and Jerry D. Coleman",Lat  :"33.4758812",Long : "-86.9173647"},{Time : "7:35 A.M.",Address :  "61st Street and Myron Massey Blvd.",Lat  :"33.4769925",Long : "-86.9094935"},{Time : "7:32 A.M.",Address :  "60th Street and Myron Massey Blvd.",Lat  :"33.4771589",Long : "-86.9093703"},{Time : "7:33 A.M.",Address :  "Myron Massey Blvd. and 55th Place",Lat  :"33.4827151",Long : "-86.9092874"},{Time : "7:35 A.M.",Address :  "Myron Massey Blvd. and 53rd Place",Lat  :"33.4823689",Long : "-86.9092869"},{Time : "7:37 A.M.",Address :  "516 54th Street (Middle of the block)",Lat  :"33.4842922",Long : "-86.9128126"},{Time : "7:41 A.M.",Address :  "54th and Avenue D",Lat  :"33.4825863",Long : "-86.9116957"},{Time : "7:43 A.M.",Address :  "5174 Hillside Drive",Lat  :"33.4868579",Long : "-86.9139871"},{Time : "7:44 A.M.",Address :  "721 Court G",Lat  :"33.5299801",Long : "-86.8849877"},{Time : "7:50 A.M.",Address :  "Glen Oaks Elementary School",Lat  :"33.4912583",Long : "-86.9104421"}];
+  var routeA_PM = [{Time : "3:45 P.M.",Address :  "Glen Oaks Elementary School",Lat  :"33.4912583",Long : "-86.9104421"},{Time : "3:46 P.M.",Address :  "5174 Hillside Drive",Lat  :"33.4868579",Long : "-86.9139871"},{Time : "3:49 P.M.",Address :  "721 Court G",Lat  :"33.5299801",Long : "-86.8849877"},{Time : "3:51 P.M.",Address :  "53rd Street and Court H",Lat  :"33.4804248",Long : "-86.9064393"},{Time : "3:52 P.M.",Address :  "Myron Massey Blvd. and Court H",Lat  :"33.482348",Long : "-86.9071609"},{Time : "3:53 P.M.",Address :  "Myron Massey Blvd. and 53rd Place",Lat  :"33.4823689",Long : "-86.9092869"},{Time : "3:54 P.M.",Address :  "Myron Massey and 55th Place",Lat  :"33.4827151",Long : "-86.9092874"},{Time : "3:57 P.M.",Address :  "516 54th Street (Middle of the block)",Lat  :"33.4842922",Long : "-86.9128126"},{Time : "3:58 P.M.",Address :  "52nd Street and Avenue D",Lat  :"33.485488",Long : "-86.91497"},{Time : "3:59 P.M.",Address :  "54th and Avenue D",Lat  :"33.4825863",Long : "-86.9116957"},{Time : "4:00 P.M.",Address :  "55th and Avenue D",Lat  :"33.482962",Long : "-86.912922"},{Time : "4:04 P.M.",Address :  "56TH Street and Terrace Avenue",Lat  :"33.4822222",Long : "-86.9186264"},{Time : "4:06 P.M.",Address :  "60th Street and Terrace Avenue",Lat  :"33.4773587",Long : "-86.9083505"},{Time : "4:07 P.M.",Address :  "Avenue C and Jerry D. Coleman",Lat  :"33.477358",Long : "-86.9061628"},{Time : "4:08 P.M.",Address :  "Avenue D and Jerry D. Coleman",Lat  :"33.4758712",Long : "-86.9159008"},{Time : "4:09 P.M.",Address :  "Court E and Jerry D. Coleman",Lat  :"33.4758549",Long : "-86.9110933"},{Time : "4:10 P.M.",Address :  "Court F and Jerry D. Coleman",Lat  :"33.4756233",Long : "-86.9099983"},{Time : "4:12 P.M.",Address :  "61st Street and Myron Massey Blvd.",Lat  :"33.476847",Long : "-86.9095797"},{Time : "4:16 P.M.",Address :  "60th Street and Myron Massey Blvd.",Lat  :"33.4771589",Long : "-86.9093703"}];
+  
+  $scope.routes = routeA_AM;
 
 });
 
